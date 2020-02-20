7baaee3eed5385dd80b94d77ad72a1b95b7035f3
justo@justo <dj.pjn16@gmail.com>
2020-02-20 18:16:08 +0300

    created a new users app, and a media folder to store the profile pictures

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts =[
- {
-    'author': 'justo',
-    'title': 'blog post 1',
-    'content': 'first post content',
-    'date_posted': 'july 27, 2019'
- },
-{
-    'author': 'peter',
-    'title': 'blog post 2',
-    'content': 'second post content',
-    'date_posted': 'july 28, 2019'
- }
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
